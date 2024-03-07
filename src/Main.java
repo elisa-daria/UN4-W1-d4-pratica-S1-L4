2ad7372cdcedd021c15141df6e54866a92e13cb5
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-03-07 15:47:59 +0100

    first-exercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import entities.Dipendente;
+
+public class Main {
+    public static void main(String[] args) {
+        String[]dipartimento={"VENDITE","PRODUZIONE","MANUTENZIONE"};
+        Dipendente a=new Dipendente(3000, new String[]{dipartimento[0]});
+        Dipendente b=new Dipendente(4000, new String[]{dipartimento[1]});
+        Dipendente c=new Dipendente(5000, new String[]{dipartimento[2]});
+        Dipendente.printDipendente(a);
+        Dipendente.printDipendente(b);
+        Dipendente.printDipendente(c);
+
+
+    }
+}
---
+++ src/entities/Dipendente.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.util.Random;
+
+public class Dipendente {
+    //attributes
+    private int matricola;
+    private double stipendio;
+    private String[] dipartimento;
+
+    //constructor
+    public Dipendente(double stipendio,String[] dipartimento) {
+        Random rdm=new Random();
+        this.matricola = rdm.nextInt(1,1000);
+        this.stipendio=stipendio;
+        this.setDipartimento(dipartimento);
+    }
+    //getters
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public String[] getDipartimento() {
+        return dipartimento;
+    }
+
+
+    //setters
+
+    public void setDipartimento(String[] dipartimento) {
+        this.dipartimento = dipartimento;
+    }
+
+    //methods
+    public static void printDipendente(Dipendente d){
+        System.out.println("MATRICOLA "+d.matricola);
+    }
+}
+
+
+
