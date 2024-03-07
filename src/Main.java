23bbb79c5e4a86d068165ebacf753624da4d40fa
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-03-07 15:53:58 +0100

    come-da-consegna

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,13 @@ public class Main {
         Dipendente a=new Dipendente(3000, new String[]{dipartimento[0]});
         Dipendente b=new Dipendente(4000, new String[]{dipartimento[1]});
         Dipendente c=new Dipendente(5000, new String[]{dipartimento[2]});
-        Dipendente.printDipendente(a);
-        Dipendente.printDipendente(b);
-        Dipendente.printDipendente(c);
-
+//        Dipendente.printDipendente(a);
+//        Dipendente.printDipendente(b);
+//        Dipendente.printDipendente(c);
+        Dipendente[]dipendenti={a,b,c};
+for (int i=0;i<dipendenti.length;i++){
+    System.out.println(dipendenti[i].getMatricola());
+}
 
     }
 }
